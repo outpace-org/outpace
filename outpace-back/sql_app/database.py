91c12e25c1f46d6632d5120d09c35cf4924b27dd
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-01-05 23:17:36 +0100

    Fixed requirements.txt

--- outpace-back/sql_app/database.py
+++ outpace-back/sql_app/database.py
@@ -3,14 +3,19 @@ from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 username = os.getenv('OUTPACE_USERNAME')
 password = os.getenv('OUTPACE_PASSWORD')
 host = os.getenv('OUTPACE_HOST')
 port = os.getenv('OUTPACE_PORT')
 DB_NAME = os.getenv('OUTPACE_DB_NAME')
 
-engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{DB_NAME}")
+SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@{host}:{port}/{DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base: DeclarativeMeta = declarative_base()
+
