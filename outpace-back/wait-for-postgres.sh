2e922c5c1974de100ca042b1cbe48a2a4a2505b2
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-01-11 16:48:02 +0100

    Added wait for postgres

---
+++ outpace-back/wait-for-postgres.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+port="$2"
+shift
+shift
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -p "$port" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
