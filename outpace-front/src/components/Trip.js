4edd393fb5d880d8f57dcf3cd797c8d5c320902e
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-02-10 18:28:12 +0100

    import

--- outpace-front/src/components/Trip.js
+++ outpace-front/src/components/Trip.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Map, GeoJson, Marker} from "pigeon-maps";
 import {
     centerZoomFromLocations, concatCoords, getGeoJsonContainingLatLng, includes, isPointInGeoJson,
