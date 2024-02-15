e120f723d60524285e4e26562600d544b07a5441
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-02-15 19:12:51 +0100

    fixed envs

--- outpace-back/sql_app/database.py
+++ outpace-back/sql_app/database.py
@@ -2,15 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 username = os.getenv('OUTPACE_USER')
 password = os.getenv('OUTPACE_PASSWORD')
 host = os.getenv('OUTPACE_HOST')
 port = os.getenv('OUTPACE_PORT')
 DB_NAME = os.getenv('OUTPACE_DB')
+
+print("connecting", username, password, host, port, DB_NAME)
 
 SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@{host}:{port}/{DB_NAME}"
 
