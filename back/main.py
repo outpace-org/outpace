94c58ce65045474f839463cbd678ad17bee58a67
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-03-07 23:00:08 +0100

    fixed main

--- back/main.py
+++ back/main.py
@@ -1,6 +1,12 @@
 import uvicorn
+from sql_app.api import init_db
+
+
+def main():
+    init_db()
+    uvicorn.run("sql_app.api:app", host="0.0.0.0", port=8000, reload=True)
 
 if __name__ == "__main__":
-    uvicorn.run("sql_app.api:app", host="0.0.0.0", port=8000, reload=True)
+    main()
 
 
