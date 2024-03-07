7e0345c6a982f2d1d10339367302f0dcf578941c
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-03-07 18:04:31 +0100

    Update db_setup.py, removed load_dotenv

--- back/tests/db_setup.py
+++ back/tests/db_setup.py
@@ -1,11 +1,11 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 def setup_function():
     # Load environment variables from your .env file
-    load_dotenv('.env')
+    #load_dotenv('.env')
 
     # Get the database credentials from environment variables
     username = os.getenv('TEST_DB_USERNAME')
