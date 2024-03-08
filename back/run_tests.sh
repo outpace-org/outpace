5d8c5ef15b750c11245a67cfb4a2b31574a1cb85
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-03-08 12:20:14 +0100

    added option to specify host in run_tests.sh

--- back/run_tests.sh
+++ back/run_tests.sh
@@ -4,9 +4,10 @@
 PASSWORD=$1
 USER=$2
 DB_NAME=$3
+HOST=$4
 
 # Restore the database
-PGPASSWORD=$PASSWORD pg_restore --verbose --clean --no-acl --no-owner -U $USER -d $DB_NAME tests/data/outpace_dump_cust.sql
+PGPASSWORD=$PASSWORD pg_restore --verbose --clean --no-acl --no-owner -h $HOST -U $USER -d $DB_NAME tests/data/outpace_dump_cust.sql
 
 # Run the tests
 pytest tests
