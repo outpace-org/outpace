b76ee72590cc4d1da7e9a7a6cd4126f0f3155ef9
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-03-09 00:15:25 +0100

    Fixed unit tests logic, don't rely on script no more

--- back/tests/db_setup.py
+++ back/tests/db_setup.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 #from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -17,5 +18,19 @@ def setup_function():
     TEST_SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@{host}:5432/{db_name}"
     test_engine = create_engine(TEST_SQLALCHEMY_DATABASE_URI)
     TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+
+    # Set the PGPASSWORD environment variable
+    env = os.environ.copy()
+    env['PGPASSWORD'] = password
+
+    # Restore the database
+    command = f'pg_restore --verbose --clean --no-acl --no-owner -h {host} -U {username} -d {db_name} tests/data/outpace_dump_cust.sql'
+    print("la commande", command)
+    try:
+        subprocess.run(command, shell=True, check=True, env=env)
+    except subprocess.CalledProcessError as e:
+        print(f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.")
+        print(f"Command output: {e.output}")
+        return None
 
     return TestSessionLocal
