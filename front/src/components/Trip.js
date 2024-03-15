a5ca6831dcb7ef7bb4425f54cc1510e4d6dac6b3
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-03-15 23:18:23 +0100

    added flag icon at trip end

--- front/src/components/Trip.js
+++ front/src/components/Trip.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Map, GeoJson, Marker } from "pigeon-maps";
+import { ReactComponent as MarkerIcon } from '../assets/flag-checkered-solid.svg';
+
 import {
   centerZoomFromLocations,
   concatCoords,
@@ -15,6 +17,7 @@ import {
 } from "../utils/functions";
 import { connect, useSelector } from "react-redux";
 import { setZoomeds } from "../actions";
+import {faStrava} from "@fortawesome/free-brands-svg-icons";
 
 var polyline = require("@mapbox/polyline");
 
@@ -50,7 +53,7 @@ function Trip({ trip, index, onButtonClick }) {
   }
   const zoomed = zoomeds[index];
   console.log("zoomed", zoomed);
-  const theClass = zoomed ? "fas fa-search-minus" : "fas fa-search-plus";
+  const zoomInOut = zoomed ? "fas fa-search-minus" : "fas fa-search-plus";
   let coordinates = [];
   let totalDistance = 0;
   let totalElevationGain = 0;
@@ -129,66 +132,75 @@ function Trip({ trip, index, onButtonClick }) {
         <p>Duration: {duration} days</p>
       </div>
       <div className="column66" style={{ position: "relative" }}>
+
         <Map
-          width={mapWidth}
-          height={mapHeight}
-          center={[centerZoom.center[1], centerZoom.center[0]]}
-          zoom={centerZoom.zoom}
-          provider={getProvider()}
-          twoFingerDrag={false}
-          mouseEvents={false}
+            width={mapWidth}
+            height={mapHeight}
+            center={[centerZoom.center[1], centerZoom.center[0]]}
+            zoom={centerZoom.zoom}
+            provider={getProvider()}
+            twoFingerDrag={false}
+            mouseEvents={false}
         >
           {geos?.map((geo, index) => (
-            <GeoJson
-              data={geo}
-              //styleCallback={() => ({ fill: `rgb(255, ${255-geoColor[1]}, ${255-geoColor[1]})` })}
-              styleCallback={() => ({
-                fill: `rgba(${(255 * (index + 1)) / geos.length}, 0, ${255 * (1 - (index + 1) / geos.length)}, .3)`,
-              })}
-            />
+              <GeoJson
+                  data={geo}
+                  styleCallback={() => ({
+                    fill: `rgba(${(255 * (index + 1)) / geos.length}, 0, ${255 * (1 - (index + 1) / geos.length)}, .3)`,
+                  })}
+              />
           ))}
 
           <GeoJson
-            data={{
-              type: "FeatureCollection",
-              features: [
-                {
-                  type: "Feature",
-                  geometry: {
-                    type: "LineString",
-                    coordinates,
+              data={{
+                type: "FeatureCollection",
+                features: [
+                  {
+                    type: "Feature",
+                    geometry: {
+                      type: "LineString",
+                      coordinates,
+                    },
+                    properties: {
+                      prop0: "value0",
+                      prop1: 0.0,
+                    },
                   },
-                  properties: {
-                    prop0: "value0",
-                    prop1: 0.0,
-                  },
-                },
-              ],
-            }}
-            styleCallback={(feature, hover) => {
-              if (feature.geometry.type === "LineString") {
-                return { strokeWidth: "2", stroke: "black" };
-              }
-              return {
-                fill: "#d4e6ec99",
-                strokeWidth: "1",
-                stroke: "white",
-                r: "20",
-              };
-            }}
+                ],
+              }}
+              styleCallback={(feature, hover) => {
+                if (feature.geometry.type === "LineString") {
+                  return { strokeWidth: "2", stroke: "black" };
+                }
+                return {
+                  fill: "#d4e6ec99",
+                  strokeWidth: "1",
+                  stroke: "white",
+                  r: "20",
+                };
+              }}
           />
 
-          {trip.activities.map(
-            (acti, index) =>
-              index < trip.activities.length - 1 &&
-              zoomed && <Marker anchor={acti.end_latlng} title={acti.name} />,
-          )}
+          {trip.activities.map((acti, index) => {
+            if (index < trip.activities.length - 1 && zoomed) {
+              return <Marker anchor={acti.end_latlng} title={acti.name} />;
+            }
+            if (index === trip.activities.length - 1 && !zoomed) {
+              return (
+                  <Marker anchor={acti.end_latlng} title={acti.name}>
+                    <MarkerIcon className="imageFlag" style={{ fill: 'black', height: '1em', width: '1em' }} />
+                  </Marker>
+              );
+            }
+            return null;
+          })}
         </Map>
+
         <button
           onClick={handleToggleClick}
           style={{ position: "absolute", top: "1em", right: "6em" }}
         >
-          <i className={theClass}></i>
+          <i className={zoomInOut}></i>
         </button>
       </div>
     </div>
