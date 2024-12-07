0c33cc83f3825a2399222ee09f918ac9bcc78499
thomas.magnienville <thomas.magnienville@exotrail.com>
2024-12-07 17:36:58 +0100

    added filtering based on distance and type of activities in dialog view

--- front/src/components/WorldMap.js
+++ front/src/components/WorldMap.js
@@ -1,126 +1,242 @@
-import React, {useEffect, useState} from "react";
-import {Map, GeoJson} from "pigeon-maps";
+import React, { useEffect, useState } from "react";
+import { GeoJson, Map } from "pigeon-maps";
 import {
-    centerZoomFromLocations,
-    concatCoords,
-    getExtremeLocations,
-    getGeoJsonContainingLatLng,
-    getProvider,
-    isPointInGeoJson,
-    index,
+  centerZoomFromLocations,
+  concatCoords,
+  getExtremeLocations,
+  getGeoJsonContainingLatLng,
+  getProvider,
+  index,
+  isPointInGeoJson,
 } from "../utils/functions";
 import polyline from "@mapbox/polyline";
 import Activities from "./Activities";
-import {Dialog, DialogContent} from "@mui/material";
+import {
+  Checkbox,
+  Dialog,
+  DialogContent,
+  FormControlLabel,
+  FormGroup,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { faBiking, faRunning } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function useGeoJson() {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        fetch("quebec.geojson")
-            .then((response) => response.json())
-            .then((jsonData) => setData(jsonData))
-            .catch((error) => console.error(error));
-    }, []);
+  useEffect(() => {
+    fetch("quebec.geojson")
+      .then((response) => response.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.error(error));
+  }, []);
 
-    return data;
+  return data;
 }
 
 function rankingAlpha(activities, index) {
-    const maxim = Math.max.apply(null, activities.map(ids => ids.length));
-    return Math.sqrt(0.1 + 0.8 * (activities[index].length / maxim));
+  const maxim = Math.max.apply(
+    null,
+    activities.map((ids) => ids.length),
+  );
+  return Math.sqrt(0.1 + 0.8 * (activities[index].length / maxim));
 }
 
-function WorldMap({activities, name}) {
-    const geoJsonQuebec = useGeoJson();
-    const [countryActivitiesOpen, setCountriesActivitiesOpen] = useState(false);
-    const [currentOpenActivity, setCurrentOpenActivity] = useState(null);
+function WorldMap({ activities, name }) {
+  const geoJsonQuebec = useGeoJson();
+  const [countryActivitiesOpen, setCountriesActivitiesOpen] = useState(false);
+  const [currentOpenActivity, setCurrentOpenActivities] = useState(null);
+  const [filters, setFilters] = useState({ running: true, cycling: true });
+  const [distanceFilter, setDistanceFilter] = useState(0); // Default: no distance filter
 
-    const handleClickOpen = (activities) => {
-        setCountriesActivitiesOpen(true);
-        setCurrentOpenActivity(activities);
-    };
+  const handleClickOpen = (activities) => {
+    setFilters({ running: true, cycling: true });
+    setDistanceFilter(0); // Reset distance filter when dialog is opened
+    setCountriesActivitiesOpen(true);
+    setCurrentOpenActivities(activities);
+  };
 
-    const handleCloseOpenActivity = () => {
-        setCountriesActivitiesOpen(false);
-    };
+  const handleCloseOpenActivity = () => {
+    setCountriesActivitiesOpen(false);
+  };
 
-    // Don't try to use geoJson until it's defined
-    if (!geoJsonQuebec) {
-        return <div>Loading...</div>;
+  const handleFilterChange = (event) => {
+    const { name, checked } = event.target;
+    setFilters((prevFilters) => ({ ...prevFilters, [name]: checked }));
+  };
+
+  const handleDistanceFilterChange = (event) => {
+    setDistanceFilter(Number(event.target.value));
+  };
+
+  const distanceFilterText =
+    distanceFilter === 0 ? "0 - ∞" : `0 - ${distanceFilter} km`;
+
+  const filteredActivities = currentOpenActivity?.filter((activity) => {
+    const matchesType =
+      (filters.running && activity.type === "Run") ||
+      (filters.cycling && activity.type === "Ride");
+    const matchesDistance =
+      distanceFilter === 0 || activity.distance / 1000 >= distanceFilter;
+    return matchesType && matchesDistance;
+  });
+  console.log("filtered activities", filteredActivities);
+
+  // Don't try to use geoJson until it's defined
+  if (!geoJsonQuebec) {
+    return <div>Loading...</div>;
+  }
+  const mapWidth = window.innerWidth * 0.8;
+  const mapHeight = mapWidth / 1.5;
+  let geos = [];
+  let combinedCoords = [];
+  let activitiesInEachCountry = [];
+  activities.forEach((activity) => {
+    try {
+      const activityCoordinates = polyline
+        .decode(activity.summary_polyline)
+        .map(([lng, lat]) => [lat, lng]);
+      const start = activityCoordinates[0];
+      let geoStart;
+      if (isPointInGeoJson(start[1], start[0], geoJsonQuebec))
+        geoStart = geoJsonQuebec;
+      else {
+        geoStart = getGeoJsonContainingLatLng(start[1], start[0]);
+      }
+      const ind = index(geos, geoStart);
+      if (ind === -1) {
+        geos.push(geoStart);
+        combinedCoords = [
+          ...combinedCoords,
+          ...getExtremeLocations(concatCoords(geoStart)),
+        ];
+        activitiesInEachCountry.push([activity]); // Store the activity ID in a new sub-array
+      } else {
+        activitiesInEachCountry[ind].push(activity); // Add the activity ID to the existing sub-array
+      }
+    } catch (error) {
+      console.error(error);
     }
-    const mapWidth = window.innerWidth * 0.8;
-    const mapHeight = mapWidth / 1.5;
-    let geos = [];
-    let combinedCoords = [];
-    let activitiesInEachCountry = [];
-    activities.forEach((activity) => {
-        try {
-            const activityCoordinates = polyline
-                .decode(activity.summary_polyline)
-                .map(([lng, lat]) => [lat, lng]);
-            const start = activityCoordinates[0];
-            let geoStart;
-            if (isPointInGeoJson(start[1], start[0], geoJsonQuebec)) geoStart = geoJsonQuebec; else {
-                geoStart = getGeoJsonContainingLatLng(start[1], start[0]);
-            }
-            const ind = index(geos, geoStart);
-            if (ind === -1) {
-                geos.push(geoStart);
-                combinedCoords = [...combinedCoords, ...getExtremeLocations(concatCoords(geoStart)),];
-                activitiesInEachCountry.push([activity]); // Store the activity ID in a new sub-array
-            } else {
-                activitiesInEachCountry[ind].push(activity); // Add the activity ID to the existing sub-array
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    });
-    console.log("geos", geos);
-    console.log("activityIds", activitiesInEachCountry); // Log the activity IDs
-    const {center, zoom} = centerZoomFromLocations(combinedCoords, mapWidth, mapHeight,);
+  });
+  console.log("geos", geos);
+  console.log("activityIds", activitiesInEachCountry); // Log the activity IDs
+  const { center, zoom } = centerZoomFromLocations(
+    combinedCoords,
+    mapWidth,
+    mapHeight,
+  );
 
-    return (<div
-        style={{
-            display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "3em",
-        }}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "3em",
+      }}
     >
-        <h1>{name} heatmap <span style={{fontSize: '0.4em'}}>(click any country for details)</span></h1>
-        <Dialog
-            open={countryActivitiesOpen}
-            onClose={handleCloseOpenActivity}
-            fullWidth={true}
-            maxWidth="md"
-            PaperProps={{
-                style: {maxHeight: '80vh'},
+      <h1>
+        {name} heatmap{" "}
+        <span style={{ fontSize: "0.4em" }}>
+          (click any country for details)
+        </span>
+      </h1>
+      <Dialog
+        open={countryActivitiesOpen}
+        onClose={handleCloseOpenActivity}
+        fullWidth={true}
+        maxWidth="md"
+        PaperProps={{
+          style: { maxHeight: "80vh" },
+        }}
+      >
+        <DialogContent>
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={filters.running}
+                  onChange={handleFilterChange}
+                  name="running"
+                />
+              }
+              label={
+                <span>
+                  <FontAwesomeIcon
+                    icon={faRunning}
+                    style={{ marginRight: "0.5em" }}
+                  />
+                </span>
+              }
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={filters.cycling}
+                  onChange={handleFilterChange}
+                  name="cycling"
+                />
+              }
+              label={
+                <span>
+                  <FontAwesomeIcon
+                    icon={faBiking}
+                    style={{ marginRight: "0.5em" }}
+                  />
+                </span>
+              }
+            />
+            <Select
+              value={distanceFilter}
+              onChange={handleDistanceFilterChange}
+              displayEmpty
+              style={{ width: "150px" }}
+              renderValue={() =>
+                distanceFilter === 0 ? "0 - 9999km" : `0 - ${distanceFilter}km`
+              }
+            >
+              <MenuItem value={0}>All distances</MenuItem>
+              <MenuItem value={10}>0 - 10km</MenuItem>
+              <MenuItem value={20}>0 - 20km</MenuItem>
+              <MenuItem value={50}>0 - 50km</MenuItem>
+              <MenuItem value={100}>0 - 100km</MenuItem>
+              <MenuItem value={200}>0 - 200km</MenuItem>
+            </Select>
+          </FormGroup>
+          <Activities activities={filteredActivities} />
+        </DialogContent>
+      </Dialog>
+
+      <Map
+        width={mapWidth}
+        height={mapHeight}
+        defaultCenter={[center[1], center[0]]}
+        defaultZoom={zoom}
+        provider={getProvider(true)}
+        twoFingerDrag={false}
+        mouseEvents={false}
+      >
+        {geos?.map((geo, index) => (
+          <GeoJson
+            key={index} // Adding a key here forces the GeoJson to re-render when the filtered activities change
+            data={geo}
+            onClick={() => {
+              console.log(
+                "the activities",
+                activitiesInEachCountry[index].map((activity) => activity.name),
+              );
+              handleClickOpen(activitiesInEachCountry[index]);
             }}
-        >
-            <DialogContent>
-                <Activities activities={currentOpenActivity}/>
-            </DialogContent>
-        </Dialog>
-        <Map
-            width={mapWidth}
-            height={mapHeight}
-            defaultCenter={[center[1], center[0]]}
-            defaultZoom={zoom}
-            provider={getProvider(true)}
-            twoFingerDrag={false}
-            mouseEvents={false}
-        >
-            {geos?.map((geo, index) => (
-                <GeoJson
-                    data={geo}
-                    onClick={() => {
-                        console.log("the activities", activitiesInEachCountry[index].map(activity => activity.name))
-                        handleClickOpen(activitiesInEachCountry[index]);
-                    }}
-                    styleCallback={() => ({
-                        fill: `rgba(255, 0, 0, ${rankingAlpha(activitiesInEachCountry, index)})`,
-                    })}
-                />))}
-        </Map>
-    </div>);
+            styleCallback={() => ({
+              fill: `rgba(255, 0, 0, ${rankingAlpha(activitiesInEachCountry, index)})`,
+            })}
+          />
+        ))}
+      </Map>
+    </div>
+  );
 }
 
 export default WorldMap;
